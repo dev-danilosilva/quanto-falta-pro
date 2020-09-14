7f84dafe671538817bb67ed05c6d15fb4c5c048e
Danilo Silva <danilo_silva@ethercity.tech>
2020-09-13 23:48:07 -0300

    FEATURE holiday selection

--- src/lib/app.js
+++ src/lib/app.js
@@ -34,7 +34,26 @@ class App{
         this._state.countdown = new Countdown();
 
         this.state.countdown.from(this.state.feriados[0].next.date).start();
+
+        const select = document.getElementById('holiday-selector');
+
+
+        for(let feriado of this.state.feriados){
+            const option = this._createElement('option', {
+                class: 'option',
+                value: feriado.name
+            });
+
+            option.innerHTML = feriado.name
+            select.appendChild(option);
+        }
+
+        select.addEventListener('change', e => {
+            const f = this.state.feriados[select.selectedIndex];
+            this.state.countdown.from(f.next.date).start();
+        });
     }
+
 
     async stop() {
         console.log('Closing Services...');
@@ -44,6 +63,15 @@ class App{
 
     get state(){
         return this._state;
+    }
+
+    _createElement(tagName, attributes={}){
+        const el = document.createElement(tagName);
+        for (const [attr, value] of Object.entries(attributes)) {
+            el.setAttribute(attr, value);
+        }
+
+        return el
     }
 
 }
--- src/lib/feriado/feriado.js
+++ src/lib/feriado/feriado.js
@@ -1,6 +1,6 @@
 class Feriado{
     constructor(label='', day=1, month=1, periodicity=1) {
-        const today= Dated.current;
+        const today = Dated.current;
         const year = today.lessThan(new Dated(day, month, today.year)) ? today.year : today.year + periodicity
         this._date = new Dated(day, month,year);
         this._label = label;
